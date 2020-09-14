2e91ea828e7d2cc297fc5bc3bdfb9ccad19dfa11
Gavin Uhran <guhran@student06.cse.nd.edu>
2020-09-13 21:42:12 -0400

    CC3 Update

--- CC3/src/Fundamental.cpp
+++ CC3/src/Fundamental.cpp
@@ -5,10 +5,46 @@
 #define ENDL std::endl
 
 int main() {
-    DoubleHash<int, std::string> hash;
-    hash.insert({76, "First"});
-    hash.insert({40, "Second"});
-    hash.insert({48, "Third"});
+    // Test 1
+    DoubleHash<int, std::string> hash1;
+    hash1.insert({76, "First"});
+    hash1.insert({40, "Second"});
+    hash1.insert({48, "Third"});
+    COUT << hash1 << ENDL;
 
-    COUT << hash << ENDL;
+
+    // Test 2
+    DoubleHash<int, std::string> hash2;
+    hash2.insert({76, "First"});
+    hash2.insert({40, "Second"});
+    hash2.insert({48, "Third"});
+    hash2.insert({5, "Fourth"});
+    hash2.insert({55, "Fifth"});
+    COUT << hash2 << ENDL;
+
+
+    // Test 3
+    DoubleHash<int, std::string> hash3;
+    hash3.insert({76, "First"});
+    hash3.insert({40, "Second"});
+    hash3.insert({48, "Third"});
+    hash3.insert({5, "Fourth"});
+    hash3.insert({55, "Fifth"});
+    hash3.insert({10, "Sixth"});
+    hash3.insert({18, "Seventh"});
+    COUT << hash3 << ENDL;
+
+
+    // Test 4
+    DoubleHash<int, std::string> hash4;
+    hash4.insert({76, "First"});
+    hash4.insert({40, "Second"});
+    hash4.insert({48, "Third"});
+    hash4.insert({5, "Fourth"});
+    hash4.insert({55, "Fifth"});
+    hash4.insert({10, "Sixth"});
+    hash4.insert({18, "Seventh"});
+    hash4.insert({44, "Eighth"});
+    hash4.insert({64, "Ninth"});
+    COUT << hash4 << ENDL;
 }
